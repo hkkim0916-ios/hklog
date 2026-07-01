8686fa8d2eb04e2ca76e4bbd817bc08e5003973e
hkkim0916-ios <hkkim0916@gmail.com>
2026-07-01 19:38:32 +0900

    fix routing structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>HKlog</h1>
-      <p>나의 기록 시스템</p>
+import { redirect } from "next/navigation";
 
-      <div style={{ marginTop: 20 }}>
-        <a href="/write">✍ 글 작성</a>
-        <br />
-        <a href="/logs">📚 기록 보기</a>
-      </div>
-    </main>
-  );
+export default function Home() {
+  redirect("/dashboard");
 }
